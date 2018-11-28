142987b6708c1f540ec9debc03817220b2f8f52a
ArtemChalov <chalov81@mail.ru>
2018-11-28 09:17:05 +0500

    Added MenuItem class

---
+++ MyPhoto/Types/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhoto.Types
+{
+    class MenuItem
+    {
+        private object _Content;
+        private string _Description;
+
+        public object Content
+        {
+            get { return Content; }
+            set { Content = value; }
+        }
+
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+    }
+}
