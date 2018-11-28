bd885073c6a0d39d4579e46e554f75a4f0db7512
ArtemChalov <chalov81@mail.ru>
2018-11-28 08:59:36 +0500

    Added FolderPresenter class

---
+++ Imgfolderpreview/ImgFolderPreviewView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Imgfolderpreview
+{
+    /// <summary>
+    /// Логика взаимодействия для ImgFolderPreviewView.xaml
+    /// </summary>
+    public partial class ImgFolderPreviewView: UserControl
+    {
+        public ImgFolderPreviewView()
+        {
+            InitializeComponent();
+            this.DataContext = new ImgFolderPreviewViewModel();
+        }
+    }
+}
