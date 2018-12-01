4bf61f08cd7ffe950dd30ec86bd51e4bd53c2592
ArtemChalov <chalov81@mail.ru>
2018-12-02 00:36:57 +0500

    Added method CreateThumbnailFromFile to the WriteableBitmapFactory class from the WriteableBitmapEx.dll.

--- WriteableBitmapEx/WriteableBitmapFactory.cs
+++ WriteableBitmapEx/WriteableBitmapFactory.cs
@@ -33,5 +33,34 @@ namespace WriteableBitmapEx
             }
             return wBitmap;
         }
+
+        public WriteableBitmap CreateThumbnailFromFile(string filePath, int width)
+        {
+            BitmapImage bmpImage = new BitmapImage();
+            WriteableBitmap wBitmap = null;
+            try
+            {
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    bmpImage.BeginInit();
+                    bmpImage.DecodePixelWidth = width;
+                    bmpImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bmpImage.StreamSource = fs;
+                    bmpImage.EndInit();
+                }
+
+                wBitmap = new WriteableBitmap(bmpImage);
+            }
+            catch
+            {
+                MessageBox.Show("Файл имеет не верный формат\nили поврежден.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                bmpImage.StreamSource = null;
+                bmpImage = null;
+            }
+            return wBitmap;
+        }
     }
 }
