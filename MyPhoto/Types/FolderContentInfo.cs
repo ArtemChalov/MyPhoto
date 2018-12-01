7c3303ce0cc5aef6928f60f6b05bb982090f9cd0
ArtemChalov <chalov81@mail.ru>
2018-12-01 23:14:23 +0500

    Added structure FolderContentInfo.

---
+++ MyPhoto/Types/FolderContentInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhoto.Types
+{
+    public struct FolderContentInfo
+    {
+        public string FileName;
+        public string FilePath;
+    }
+}
