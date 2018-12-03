d3de015b86cef33de267d87a0e71ecabfbad3fd3
ArtemChalov <chalov81@mail.ru>
2018-12-03 11:59:04 +0500

    Removed OnPropertyChanged() from SelectedPreviewImage property.

--- MyPhoto/MainWindow.xaml.cs
+++ MyPhoto/MainWindow.xaml.cs
@@ -166,7 +166,6 @@ namespace MyPhoto
                 _SelectedPreviewImage = value;
                 if (value.FilePath != null)
                     FilePath = value.FilePath;
-                OnPropertyChanged();
             }
         }
 
